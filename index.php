bada32cff9b8c7beeea83da8431f0b03e0d8aeb0
Christopher Collopy <chelco91@gmail.com>
2015-07-13 16:55:19 -0600

    Added css directory and styles.css file Inside index.php added new paragraph

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<p> Hello, my name is Kenneth Chavez. I am graphic designer by trade who really wants to learn
 			coding and give up my part time job as ferret trainer. Training ferrets can be a a complex job since
 			ferrets vocal box is 3mm thick.</p>
-		<img src="images/kchavez68.jpg">
+		<img src="images/kchavez68.jpg" width="720" height="719">
 		<p>My second life as a pool cleaner has been wrought with life lessons. I've learned plenty about chemicals and their applications.</p>
 
 	</body>
