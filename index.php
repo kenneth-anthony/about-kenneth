bd9c9bfd1151b11abfc34050d35870809d67c5ab
Kenneth Chavez <kchavez.bills@gmail.com>
2015-07-13 15:58:47 -0600

    added gitignore added HTML and img src renamed image src to have lower case letters

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-	<html>
+<html>
 	<body>
 
 		<h1> About Kenneth</h1>
 
-		<p> Hello, my name is Kenneth Chavez. I am graphic designer by trade, who really wants to learn
+		<p> Hello, my name is Kenneth Chavez. I am graphic designer by trade who really wants to learn
 			coding and give up my part time job as ferret trainer. Training ferrets can be a a complex job since
-			ferrets vocal box is 3mm thick.;/</P>
+			ferrets vocal box is 3mm thick./</P>
 		<img src="images/kchavez68.jpg">
 
-	</body>
+		</body>
 	</html>
