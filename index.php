61a73644b769f9d879ff00e169727101bb2bba9c
Kenneth Chavez <kchavez.bills@gmail.com>
2015-07-13 15:19:39 -0600

    added gitignore added HTML and img src

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+	<html>
+	<body>
+		<h1> About Kenneth</h1>
+		<p> Hello, my name is Kenneth Chavez. I am graphic designer by trade, who really wants to learn <b>
+				coding and give up my part time job as ferret trainer.;>
+				<img src="IMG_kchavez68.JPG">
+</html>
