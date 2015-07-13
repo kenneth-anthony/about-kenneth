c10f549a2f8e85f639c13dcb76cf82de0dde45da
Christopher Collopy <chelco91@gmail.com>
2015-07-13 16:43:56 -0600

    Added css directory and styles.css file Inside index.php added new paragraph

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 			coding and give up my part time job as ferret trainer. Training ferrets can be a a complex job since
 			ferrets vocal box is 3mm thick.</P>
 		<img src="images/kchavez68.jpg">
+		<p>My second life as a pool cleaner has been wrought with life lessons. I've learned plenty about chemicals and their application</p>
 
 		</body>
 	</html>
